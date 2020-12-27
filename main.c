7cee8ac7c83a9c2350b8a0d9789d7ec45b5bed30
ishay320 <ishay320@hotmail.com>
2020-12-27 19:58:59 +0200

    first q

--- main.c
+++ main.c
@@ -1,8 +1,50 @@
+#include <stdio.h>
 
-
+void shift_element(int *arr, int i);
+void insertion_sort(int *arr, int len);
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int arr[] = {};
+    insertion_sort(arr, sizeof(arr) / sizeof(int));
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++)
+    {
+        printf("%d, ", arr[i]);
+    }
+
     return 0;
+}
+
+void shift_element(int *arr, int i)
+{
+    int next_num = 0;
+
+    if (i != 0)
+    {
+        next_num = *(arr + 1);
+        *(arr + 1) = *(arr);
+        *arr = 0;
+    }
+
+    for (int j = 1; j < i; j++)
+    {
+        int temp = *(arr + j + 1);
+        *(arr + j + 1) = next_num;
+        next_num = temp;
+    }
+}
+
+void insertion_sort(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        int pos = i;
+        while (pos > 0 && *(arr + i ) < *(arr + (pos - 1)))
+        {
+            pos--;
+        }
+        int temp = *(arr + i );
+        shift_element(arr + pos , i - pos);
+        *(arr + pos) = temp;
+    }
 }
